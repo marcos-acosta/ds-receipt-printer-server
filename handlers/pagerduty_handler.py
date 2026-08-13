57c4a9f3e5a6f58fb52e5fccbfb0156e19ea8f0b
Marcos Acosta <macosta1625@gmail.com>
2026-08-13 10:29:15 -0400

    Better PagerDuty data

--- handlers/pagerduty_handler.py
+++ handlers/pagerduty_handler.py
@@ -5,13 +5,21 @@ from util import HR_STRAIGHT, pad_newlines, print_timestamp, strip_markdown_link
 
 class PagerDutyHandler(WebhookBodyHandler):
     def generatePrintables(self, message: dict) -> list[Printable] | None:
+        print(message, flush=True)
         event = message.get("event", {})
+        data = message.get("data", {})
+        title = data.get("title", "(no title)")
+        assignees = data.get("assignees", [])
+        assignee = (
+            assignees[0].get("summary", "[?]") if len(assignees) else "(no assignee)"
+        )
+        urgency = data.get("urgency", "(no urgency)")
+        service = data.get("service", {}).get("summary", "(no service)")
         occurred_at = event.get("occurred_at", "")
         occurred_at_pretty = (
             print_timestamp(occurred_at) if occurred_at else "(no timestamp)"
         )
-        message_body = strip_markdown_links(event.get("data", {}).get("message", ""))
-        text = f"HELIUM BACKEND ALERT\nTriggered at {occurred_at_pretty}\n{HR_STRAIGHT}\n{message_body}\n"
+        text = f"PAGERDUTY ALERT // {service}\n{title}\n{HR_STRAIGHT}\nTriggered at {occurred_at_pretty}\nAssigned to {assignee}\nUrgency: {urgency}"
         return [
             Text(pad_newlines(text, 8)),
             CutAndPrint(),
