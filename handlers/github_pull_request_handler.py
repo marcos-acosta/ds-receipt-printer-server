b434f59d221394107eda58818613d8138ec65f4c
Marcos Acosta <macosta1625@gmail.com>
2026-08-17 16:01:41 -0400

    Fixes for github handler

--- handlers/github_pull_request_handler.py
+++ handlers/github_pull_request_handler.py
@@ -6,34 +6,22 @@ from util import HR_WAVY, print_timestamp
 class GitHubPullRequestHandler(WebhookBodyHandler):
     def generatePrintables(self, message: dict) -> list[Printable] | None:
         action = message.get("action", "")
-        if action != "review_requested":
+        if action != "review_requested" or "requested_reviewer" not in message:
             return None
         number = message.get("number", "???")
         pr = message.get("pull_request", {})
         title = pr.get("title", "(no title)")
         sender = pr.get("user", {}).get("login", "(no sender)")
         body = pr.get("body", "(no body)")
-        requested_reviewers = pr.get("requested_reviewers", [])
-        reviewer_names = (
-            ", ".join([r.get("login", "(no name)") for r in requested_reviewers])
-            if requested_reviewers
-            else "(no requested reviewers)"
+        requested_reviewer = message.get("requested_reviewer", {}).get(
+            "login", "(no requested reviewer)"
         )
         head = pr.get("head", {})
         branch = head.get("ref", "(no branch name)")
-        repo_name = head.get("repo", {}).get("full name", "(no repo name)")
+        repo_name = head.get("repo", {}).get("full_name", "(no repo name)")
         created_at = pr.get("created_at", "")
         created_at_pretty = (
             print_timestamp(created_at) if created_at else "(no timestamp)"
         )
-        message_text = f"""
-        Pull request #{number} opened by {sender}
-        {repo_name} / {branch}
-        {title}
-
-        Created at {created_at_pretty}       
-        Review requested from: {reviewer_names}
-        {HR_WAVY}
-        {body}
-        """.strip()
-        return [Text("\n\n\n" + message_text), CutAndPrint()]
+        message_text = f"\n\n\nPull request #{number} opened by {sender}\n{repo_name} / {branch}\n{title}\n\nCreated at {created_at_pretty}\nReview requested from {requested_reviewer}\n{HR_WAVY}\n{body}"
+        return [Text(message_text), CutAndPrint()]
